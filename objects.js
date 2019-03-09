8cb5bf559da772387724726da409f1c5ef8bfee4
Stephen Brewer <stephenbrewer16@gmail.com>
2019-03-09 01:30:04 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,6 +5,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete.[artistName]
+  delete.songTitle
   return playlist
 }
